9b627801cd0c9b633dc57a1323b720bf5a1ac4d3
belaeat <omyhossain@gmail.com>
2023-03-31 20:18:37 +0600

    working on blog component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css'
 import Header from './components/Header/Header'
 import Blog from './components/Blog/Blog'
 import QnA from './components/QnA/QnA'
+import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
   return (
     <div className="App m-10">
       <Header></Header>
       <Blog></Blog>
+      <Sidebar></Sidebar>
       <QnA></QnA>
     </div>
   )
--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Sidebar from '../Sidebar/Sidebar';
 
 const Blog = () => {
     return (
-        <div>
-            
+        <div className='flex gap-5 my-10'>
+            <div className=' w-[75%] bg-black'>
+                <h2 className='text-white'>This is Blog space</h2>
+
+                
+
+            </div>
+            <div className='w-[25%] bg-slate-600'>
+                <h1 className='text-white'>This is Sidebar</h1>
+                <Sidebar></Sidebar>
+            </div>
         </div>
     );
 };
